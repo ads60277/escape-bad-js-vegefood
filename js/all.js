a2eb09b7831dc11eb3ee46e2293dcdb75f76e7e5
ads60277 <ads60277@gmail.com>
2022-04-28 14:06:01 +0800

    fix

--- js/all.js
+++ js/all.js
@@ -1,12 +1,13 @@
 // TODO: 修正 ESLint 錯誤、補上分號、前輩說要改單引號 QQ
 const url = 'https://hexschool.github.io/js-filter-data/data.json';
-const table = document.querySelector('.table-content');
-let data = [];
+let data;
+/* global axios */
 
+const table = document.querySelector('.table-content');
 function renderData(input) {
   let str = '';
   input.forEach((b) => {
-  // TODO: 改成 ES6 的 Template Literals (字面字串符)
+    // TODO: 改成 ES6 的 Template Literals (字面字串符)
     const content = `<tr><td>${b.作物名稱
     }</td><td>${b.市場名稱
     }</td><td>${b.上價
@@ -37,20 +38,7 @@ function filterCategory(e) {
     category = e.target.dataset.category;
     showData = data.filter((i) => i.種類代碼 === category);
     // TODO: 之後拆成 renderData 函式
-    let str = '';
-    showData.forEach((i) => {
-      const content = `<tr><td>${i.作物名稱
-      }</td><td>${i.市場名稱
-      }</td><td>${i.上價
-      }</td><td>${i.中價
-      }</td><td>${i.下價
-      }</td><td>${i.平均價
-      }</td><td>${i.交易量
-      }</td></tr>`;
-      str += content;
-    });
-    table.innerHTML = str;
+    renderData(showData);
   }
 }
-
 filter.addEventListener('click', filterCategory);
